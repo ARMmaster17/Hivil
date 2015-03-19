788746832e48fef44156386440141e42ba6dd24a
Joshua Zenn <wordman05@gmail.com>
2015-03-19 12:01:31 -0400

    Update Class1.cs

--- hiveapi/Class1.cs
+++ hiveapi/Class1.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace hiveapi
 {
     public class APIobject
     {
-        HIVEvar<int> hv = new HIVEvar<int>();
-        public APIobject()
+        //HIVEvar<int> hv = new HIVEvar<int>();
+        int api_PROCESSID;
+        IPAddress api_HOSTIP;
+        int api_HOSTPORT;
+        public APIobject(string hostIP, int hostPort, int processID)
         {
-            hv.value = 5;
-            List<int> i;
+            api_HOSTIP = IPAddress.Parse(hostIP);
+            api_HOSTPORT = hostPort;
+            api_PROCESSID = processID;
         }
     }
     public class HIVEvar<T>
     {
+        public string name;
         public T value
         {
             get
@@ -25,12 +31,17 @@ namespace hiveapi
             }
             set
             {
-
+                IPEndPoint endPoint = new IPEndPoint(Ipaddress.Parse("127.0.0.1"), 8000);
+                Socket serverConn = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                serverConn.Connect(endPoint);
+                serverConn.send(Encoding.ASCII.GetBytes("SET|" + name + "|" /*value*/));
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
             }
         }
-        public HIVEvar()
+        public HIVEvar(string vname)
         {
-
+            name = vname;
         }
     }
 }
